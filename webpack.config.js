115daca89fae6b2d9a6759211ba360bcdc4166cc
attributeofextension <leah.carr00@gmail.com>
2018-01-15 19:20:22 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+//webpack.config.js
+
+module.exports = {
+    entry: ['./app/main.js'],
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
